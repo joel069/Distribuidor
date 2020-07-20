369ee506e05a2581c2e2c1b74733e28f32368d1b
Kevin Cordero <46872883+wonejo92@users.noreply.github.com>
2020-07-20 18:44:22 -0500

    Pedidos Clase e interfaz

---
+++ Distribuidor/src/ec/edu/ups/controlador/PedidoCabeceraBeans.java
@@ -0,0 +1,28 @@
+package ec.edu.ups.controlador;
+
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.inject.Named;
+
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+@Named
+@SessionScoped
+public class PedidoCabeceraBeans implements Serializable {
+
+
+
+	private static final long serialVersionUID = 1L;
+	@EJB private PedidoCabeceraBeans ejbCabeceraBeans;
+	private String fecha;
+	private double total;
+	private String estadoPedido;
+	
+
+	public PedidoCabeceraBeans() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
