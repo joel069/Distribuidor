9e099cc8b85376fae616f44c5aa82cdc311ca015
Kevin Cordero <46872883+wonejo92@users.noreply.github.com>
2020-07-21 13:36:18 -0500

    CSS y Funcionalidad

--- Distribuidor/src/ec/edu/ups/controlador/PedidosDetallesBeans.java
+++ Distribuidor/src/ec/edu/ups/controlador/PedidosDetallesBeans.java
@@ -1,6 +1,7 @@
 package ec.edu.ups.controlador;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -9,9 +10,14 @@ import javax.enterprise.context.SessionScoped;
 import javax.faces.annotation.FacesConfig;
 import javax.inject.Named;
 
+import ec.edu.ups.ejb.FacturaCabeceraFacade;
+import ec.edu.ups.ejb.FacturaDetalleFacade;
 import ec.edu.ups.ejb.PedidoCabeceraFacade;
 import ec.edu.ups.ejb.PedidosDetallesFacade;
+import ec.edu.ups.ejb.ProductoFacade;
 import ec.edu.ups.ejb.UsuarioFacade;
+import ec.edu.ups.modelo.FacturaCabecera;
+import ec.edu.ups.modelo.FacturaDetalle;
 import ec.edu.ups.modelo.PedidoDetalle;
 import ec.edu.ups.modelo.PedidosCabecera;
 import ec.edu.ups.modelo.Persona;
@@ -28,6 +34,10 @@ public class PedidosDetallesBeans implements Serializable {
 	@EJB PedidosDetallesFacade ejbPedidosDetallesFacade;
 	@EJB PedidoCabeceraFacade  ejbPedidoCabeceraFacade;
 	@EJB UsuarioFacade  ejbUsuarioFacade;
+	@EJB FacturaCabeceraFacade ejbFacturaCabeceraFacade;
+	@EJB FacturaDetalleFacade ejbFacturaDetalleFacade;
+	@EJB UsuarioFacade ejbUsuarioFacade2;
+	@EJB ProductoFacade ejbProductoFacade;
 	
 	private int cantidad;
 	private double subtotalDetalle;
@@ -35,7 +45,12 @@ public class PedidosDetallesBeans implements Serializable {
 	private List<Producto> listProductos;
 	private List<PedidoDetalle> listPedidosDetalles;
 	private List<PedidosCabecera> listPeidodsCabeceras;
-	private String correo;
+	private String correo; 
+	private FacturaCabecera cabecera;
+	private String NombreCliente;
+	
+	//Variables para el for normal y la cabecera 
+	private PedidoDetalle pedidoDetalle;
 	
 
 	public PedidosDetallesBeans() {
@@ -125,6 +140,41 @@ public class PedidosDetallesBeans implements Serializable {
 	
 	public String Facturar() {
 		System.out.println("Se procede a facturar el pedido");
+		List<PedidoDetalle> PedidoDetalleList = ejbPedidosDetallesFacade.pedidosDetallesFacturacion();
+		System.out.println(PedidoDetalleList);
+		String estado="A";
+		
+		
+	for (PedidoDetalle pedidoDetalle : PedidoDetalleList) {
+		System.out.println("Fecha"+pedidoDetalle.getPedidosCabID().getFecha());
+		System.out.println("TOtal"+pedidoDetalle.getPedidosCabID().getTotal());
+		System.out.println("CantidadProducto"+pedidoDetalle.getCantidad());
+		System.out.println("Cliente cedula "+pedidoDetalle.getPedidosCabID().getCliente().getCedula());
+	
+		
+		Usuario usuario = ejbPedidosDetallesFacade.BUscarBycedula(pedidoDetalle.getPedidosCabID().getCliente().getCedula());
+		Producto producto =  ejbProductoFacade.find(pedidoDetalle.getProid().getId());
+		
+		NombreCliente=usuario.getNombre();
+		System.out.println("Nombre del cliente******"+NombreCliente);
+		cabecera = new FacturaCabecera(pedidoDetalle.getPedidosCabID().getFecha(),
+				pedidoDetalle.getPedidosCabID().getTotal(),estado,usuario);
+		
+		FacturaDetalle detalle = new FacturaDetalle(pedidoDetalle.getCantidad(),pedidoDetalle.getSubtotalDetalle(),
+			pedidoDetalle.getPedidosCabID().getTotal(),0.0,cabecera,producto);
+		
+		System.out.println("FACTURA CABEZERA---->"+cabecera.toString());
+		System.out.println("FACTURA DETALLE----->"+detalle.toString());
+		//System.out.println("Producto recuperado por le ID--->"+producto);
+		
+		//Se procede Persistir en la BD
+		ejbFacturaCabeceraFacade.create(cabecera);
+		ejbFacturaDetalleFacade.create(detalle);
+		
+	}
+		
+		
+		
 		return "funciona";
 	}
 
