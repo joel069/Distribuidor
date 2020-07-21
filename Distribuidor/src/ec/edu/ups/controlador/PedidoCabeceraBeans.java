7358c56abc930a7109edffc1cde327ecdd1a8ad3
Kevin Cordero <46872883+wonejo92@users.noreply.github.com>
2020-07-20 19:04:50 -0500

    Clase Pedidos Agregada.

--- Distribuidor/src/ec/edu/ups/controlador/PedidoCabeceraBeans.java
+++ Distribuidor/src/ec/edu/ups/controlador/PedidoCabeceraBeans.java
@@ -1,11 +1,18 @@
 package ec.edu.ups.controlador;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.annotation.FacesConfig;
 import javax.inject.Named;
+
+import ec.edu.ups.ejb.PedidoCabeceraFacade;
+import ec.edu.ups.ejb.PedidosDetallesFacade;
+import ec.edu.ups.modelo.PedidosCabecera;
+import ec.edu.ups.modelo.Usuario;
 
 @FacesConfig(version = FacesConfig.Version.JSF_2_3)
 @Named
@@ -13,17 +20,55 @@ import javax.inject.Named;
 public class PedidoCabeceraBeans implements Serializable {
 
 
-	
-
 	private static final long serialVersionUID = 1L;
-	@EJB private PedidoCabeceraBeans ejbCabeceraBeans;
+	@EJB private PedidoCabeceraFacade ejbCabeceraFacade;
 	private String fecha;
 	private double total;
 	private String estadoPedido;
-	
+	private List<PedidosCabecera> listPedidosCabecera;
+	private List<Usuario> personalist;
+
 
 	public PedidoCabeceraBeans() {
-		// TODO Auto-generated constructor stub
+
 	}
+	@PostConstruct
+	public void init() {
+		listPedidosCabecera = ejbCabeceraFacade.findAll();
+	}
+	
+//Getters And Setters
+	public String getFecha() {
+		return fecha;
+	}
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	public String getEstadoPedido() {
+		return estadoPedido;
+	}
+	public void setEstadoPedido(String estadoPedido) {
+		this.estadoPedido = estadoPedido;
+	}
+	public List<PedidosCabecera> getListPedidosCabecera() {
+		return listPedidosCabecera;
+	}
+	public void setListPedidosCabecera(List<PedidosCabecera> listPedidosCabecera) {
+		this.listPedidosCabecera = listPedidosCabecera;
+	}
+	public List<Usuario> getPersonalist() {
+		return personalist;
+	}
+	public void setPersonalist(List<Usuario> personalist) {
+		this.personalist = personalist;
+	}
+	
+	
 
 }
---
+++ Distribuidor/src/ec/edu/ups/controlador/PedidosDetallesBeans.java
@@ -0,0 +1,95 @@
+package ec.edu.ups.controlador;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.annotation.FacesConfig;
+import javax.inject.Named;
+
+import ec.edu.ups.ejb.PedidoCabeceraFacade;
+import ec.edu.ups.ejb.PedidosDetallesFacade;
+import ec.edu.ups.modelo.PedidoDetalle;
+import ec.edu.ups.modelo.PedidosCabecera;
+import ec.edu.ups.modelo.Producto;
+
+@FacesConfig(version = FacesConfig.Version.JSF_2_3)
+@Named
+@SessionScoped
+public class PedidosDetallesBeans implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@EJB PedidosDetallesFacade ejbPedidosDetallesFacade;
+	@EJB PedidoCabeceraFacade  ejbPedidoCabeceraFacade;
+	
+	
+	private int cantidad;
+	private double subtotalDetalle;
+	private double total;
+	private List<Producto> listProductos;
+	private List<PedidoDetalle> listPedidosDetalles;
+	private List<PedidosCabecera> listPeidodsCabeceras;
+	
+
+	public PedidosDetallesBeans() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@PostConstruct
+	public void init() {
+		listPedidosDetalles=ejbPedidosDetallesFacade.findAll();
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getSubtotalDetalle() {
+		return subtotalDetalle;
+	}
+
+	public void setSubtotalDetalle(double subtotalDetalle) {
+		this.subtotalDetalle = subtotalDetalle;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public List<Producto> getListProductos() {
+		return listProductos;
+	}
+
+	public void setListProductos(List<Producto> listProductos) {
+		this.listProductos = listProductos;
+	}
+
+	public List<PedidoDetalle> getListPedidosDetalles() {
+		return listPedidosDetalles;
+	}
+
+	public void setListPedidosDetalles(List<PedidoDetalle> listPedidosDetalles) {
+		this.listPedidosDetalles = listPedidosDetalles;
+	}
+
+	public List<PedidosCabecera> getListPeidodsCabeceras() {
+		return listPeidodsCabeceras;
+	}
+
+	public void setListPeidodsCabeceras(List<PedidosCabecera> listPeidodsCabeceras) {
+		this.listPeidodsCabeceras = listPeidodsCabeceras;
+	}
+	
+
+}
